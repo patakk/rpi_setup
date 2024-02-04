3c49d2e6ce01531bdef95c6fd26abafe49f78a3e
Paolo RPi <paolo.ceric@gmail.com>
2024-02-04 19:13:51 +0100

    changes

--- setup.sh
+++ setup.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-#!/bin/bash
 
 echo "Updating and upgrading system..."
 sudo apt-get update -y > /dev/null 2>&1
 sudo apt-get upgrade -y > /dev/null 2>&1
 
-echo "Installing curl, git, fish, and vim..."
-sudo apt-get install -y curl git fish vim > /dev/null 2>&1
+echo "Installing curl, git, fish, tmux, and vim..."
+sudo apt-get install -y curl git fish vim, tmux > /dev/null 2>&1
 
 echo "Installing Pimoroni Inky..."
-curl https://get.pimoroni.com/inky | bash > /dev/null 2>&1
+curl -sS https://get.pimoroni.com/inky | bash > /dev/null 2>&1
 
 echo "Cloning Inky library from GitHub..."
 git clone https://github.com/pimoroni/inky.git > /dev/null 2>&1
@@ -18,4 +17,9 @@ cd inky
 echo "Installing Inky library..."
 pip install inky > /dev/null 2>&1
 
+echo "Setting fish as the default shell..."
+chsh -s /usr/bin/fish
+fish
+
 echo "Installation completed!"
+
