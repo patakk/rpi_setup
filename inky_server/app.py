bdfbe3557cb91bb2eb95fecac6723495d6658701
paolo rpi <paolo.ceric@gmail.com>
2024-02-06 05:28:57 +0100

    changes

--- inky_server/app.py
+++ inky_server/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, render_template, redirect, jsonify
 from werkzeug.utils import secure_filename
-from PIL import Image
 import uuid
 import os
+import base64
 from shared import allowed_file
 from shared import update_display
 from shared import create_thumbnail
@@ -32,7 +32,7 @@ def gallery():
     return render_template('gallery.html', images=thumbnails)
 
 @app.route('/canvas')
-def gallery():
+def canvas():
     return render_template('canvas.html')
 
 
@@ -59,14 +59,43 @@ def display_image(filename):
             f.write(str(current_idx))
 
         update_display(image_path)
-        return redirect('/gallery')
+        return jsonify({'success': 'Image changed'}), 200
     else:
         return jsonify({'error': 'File does not exist'}), 404
 
 
 @app.route('/', methods=['GET'])
-def upload_form():
+def mainpage():
     return render_template('index.html')
+
+@app.route('/upload_canvas', methods=['POST'])
+def upload_canvas_image():
+    data = request.json
+    if not data or 'image' not in data:
+        return jsonify({'error': 'No image data'}), 400
+
+    # Strip the header of base64 string
+    image_data = data['image'].split(';base64,')[-1]
+
+    try:
+        image_bytes = base64.b64decode(image_data)
+        filename = secure_filename(str(uuid.uuid4()) + '.png')
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        with open(file_path, 'wb') as f:
+            f.write(image_bytes)
+        
+        create_thumbnail(file_path)
+        
+        with open('../current_img_idx', 'w') as f:
+            f.write(str(len(os.listdir(UPLOAD_FOLDER)) - 1))
+        
+        update_display(file_path)
+        print('canvas image saved')
+        return jsonify({'success': 'Image saved', 'filename': filename}), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -87,6 +116,7 @@ def upload_file():
 
         update_display(file_path)
         return redirect('/')
+        # return jsonify({'success': 'Image uploaded'}), 200
 
 if __name__ == '__main__':
     initialize()
--- inky_server/static/script.js
+++ inky_server/static/script.js
@@ -4,3 +4,81 @@ function doLabel(){
     document.getElementById("synclabel").style.opacity = 1.;
     setTimeout(()=>{document.getElementById("synclabel").style.opacity = 0.}, 15000);
 }
+
+
+const DESATURATED_PALETTE = [
+    [0, 0, 0],
+    [255, 255, 255],
+    [0, 255, 0],
+    [0, 0, 255],
+    [255, 0, 0],
+    [255, 255, 0],
+    [255, 140, 0],
+  ];
+  
+let canvas;
+let ctx;
+let currentBrushColorIndex = 0;
+let currentBgColorIndex = 1;
+let brushColor = `rgb(${DESATURATED_PALETTE[currentBrushColorIndex].join(',')})`;
+let bgColor = `rgb(${DESATURATED_PALETTE[currentBgColorIndex].join(',')})`;
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'q') {
+        currentBrushColorIndex = (currentBrushColorIndex + 1) % DESATURATED_PALETTE.length;
+        brushColor = `rgb(${DESATURATED_PALETTE[currentBrushColorIndex].join(',')})`;
+    }
+    if (event.key === 'e') {
+        currentBgColorIndex = (currentBgColorIndex + 1) % DESATURATED_PALETTE.length;
+        bgColor = `rgb(${DESATURATED_PALETTE[currentBgColorIndex].join(',')})`;
+        ctx.fillStyle = bgColor;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+    if (event.key === 'c') {
+        ctx.fillStyle = bgColor;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+  
+    if(!canvas)
+        return;
+
+    ctx.fillStyle = bgColor;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  
+    let isDrawing = false;
+  
+    function startDrawing(e) {
+      isDrawing = true;
+      draw(e); // This is to ensure the ellipse is drawn even if the mouse is not moved
+    }
+  
+    function draw(e) {
+      if (!isDrawing) return;
+  
+      const rect = canvas.getBoundingClientRect();
+      const x = e.clientX - rect.left; // Get the x-coordinate relative to the canvas
+      const y = e.clientY - rect.top; // Get the y-coordinate relative to the canvas
+  
+      // Draw an ellipse at the mouse position
+      ctx.fillStyle = brushColor;
+      console.log(brushColor);
+      ctx.fillRect(x, y, 44, 44);
+    }
+  
+    function stopDrawing() {
+      isDrawing = false;
+    }
+  
+    canvas.addEventListener('mousedown', startDrawing);
+    canvas.addEventListener('mousemove', draw);
+    canvas.addEventListener('mouseup', stopDrawing);
+    canvas.addEventListener('mouseout', stopDrawing); // Stop drawing when the mouse leaves the canvas
+  });
+  
